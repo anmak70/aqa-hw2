547606b556723a8291f1faf0f71ea7ef0e6510c2
Andrey Makolov <anmak70@mail.ru>
2019-12-08 23:49:24 +0300

    parameterization of tests

--- src/test/java/ru/netology/hw2/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/hw2/CashbackHackServiceTest.java
@@ -1,53 +1,18 @@
 package ru.netology.hw2;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CashbackHackServiceTest {
 
-    @Test
-    void shouldReturn100IfAmountIs900() {
+    @ParameterizedTest
+    @CsvFileSource(resources = "/AmountData.csv", numLinesToSkip = 1)
+    void shouldReturnAmountPriorToPurchaseForCashback(int amount, int expected, String message) {
         CashbackHackService cashbackHackService = new CashbackHackService();
-        int amount = 900;
         int actual = cashbackHackService.remain(amount);
-        int expectid = 100;
-        assertEquals(expectid, actual);
+        assertEquals(expected, actual);
     }
 
-    @Test
-    void shouldReturn1IfAmountIs999() {
-        CashbackHackService cashbackHackService = new CashbackHackService();
-        int amount = 999;
-        int actual = cashbackHackService.remain(amount);
-        int expectid = 1;
-        assertEquals(expectid, actual);
-    }
-
-    @Test
-    void shouldReturn0IfAmountIs1000() {
-        CashbackHackService cashbackHackService = new CashbackHackService();
-        int amount = 1000;
-        int actual = cashbackHackService.remain(amount);
-        int expectid = 0;
-        assertEquals(expectid, actual);
-    }
-
-    @Test
-    void shouldReturn999IfAmountIs1001() {
-        CashbackHackService cashbackHackService = new CashbackHackService();
-        int amount = 1001;
-        int actual = cashbackHackService.remain(amount);
-        int expectid = 999;
-        assertEquals(expectid, actual);
-    }
-
-    @Test
-    void shouldReturn0IfAmountIs2000() {
-        CashbackHackService cashbackHackService = new CashbackHackService();
-        int amount = 2000;
-        int actual = cashbackHackService.remain(amount);
-        int expectid = 0;
-        assertEquals(expectid, actual);
-    }
 }
